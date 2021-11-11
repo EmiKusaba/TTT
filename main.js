d1aa06aa23013cc8a09cc066fbdae59e7078bc36
EmiKusaba <eeemi615@gmail.com>
2021-11-11 14:09:53 -0600

    added checkWhoWin func

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 let originalBoard = [0,1,2,3,4,5,6,7,8];
 let player1 = "X";
 let player2 = "O";
+let pions = ["", "X", "O"];
 let count = 0;
+let currentPlayer = 1;
 
+
+const play = (e) => {
+  currentElement = e;
+  if (isEmpty(e)) {
+    e.innerHTML = pions[currentPlayer];
+    currentPlayer = (currentPlayer == 1) ? 2 : 1;
+    if (checkWin(e) != "") {
+      endGame(checkWhoWin(e));
+    }
+  }
+}
 //function mark x or o
 const addMarker = () => {
-  const currentMarker = document.getElementById
+  const currentMarker = document.getElementsByTagName("button").value
 }
 //win state
 const checkWin = (record, player) => {
@@ -29,8 +42,16 @@ const checkWin = (record, player) => {
 }
 //draw state
 const checkDraw = () => {
-  if (moves === 9){
+  if (count === 9){
     draw();
+  }
+}
+//check win
+ const checkWorWin = () => {
+  if(checkWin()===true) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
   }
 }
 //clear button
