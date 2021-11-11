5a4cb29623e898a80b87e782f18c9fb8e69f16c2
EmiKusaba <eeemi615@gmail.com>
2021-11-11 16:03:35 -0600

    final fix

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
-let originalBoard = [0,1,2,3,4,5,6,7,8];
 let player1 = "X";
 let player2 = "O";
-let pions = ["", "X", "O"];
-let count = 0;
-let currentPlayer = 1;
+let currentPlayer = player1;
 
 
 const play = (e) => {
-  currentElement = e;
-  if (isEmpty(e)) {
-    e.innerHTML = pions[currentPlayer];
-    currentPlayer = (currentPlayer == 1) ? 2 : 1;
-    if (checkWin(e) != "") {
-      endGame(checkWhoWin(e));
-    }
+  // currentElement = e;
+  // if (isEmpty(e)) {
+  //   e.innerHTML = pions[currentPlayer];
+  //   currentPlayer = (currentPlayer == 1) ? 2 : 1;
+  //   if (checkWin(e) != "") {
+  //     endGame(checkWhoWin(e));
+  //   }
+  // }
+
+  e.innerHTML = currentPlayer
+
+  if(checkWin(currentPlayer)) {
+    window.alert(`Player ${currentPlayer} won!`)
+  } else if(checkDraw()) {
+    window.alert("It's a draw!")
   }
+
+  currentPlayer = currentPlayer === player1 ? player2 : player1
 }
-//function mark x or o
-const addMarker = () => {
-  const currentMarker = document.getElementsByTagName("button").value
-}
+
 //win state
-const checkWin = (record, player) => {
+const checkWin = (player) => {
+  record = []
+  for (let i = 0; i < 9; ++i) {
+    const e = document.getElementById(String(i))
+    record.push(e.innerHTML)
+  }
+
   if (
   //horizonal win
   (record[0] === player && record[1] === player && record[2] === player) ||
@@ -40,18 +50,15 @@ const checkWin = (record, player) => {
     return false;
   }
 }
+
 //draw state
 const checkDraw = () => {
-  if (count === 9){
-    draw();
+  for (let i = 0; i < 9; ++i) {
+    const e = document.getElementById(String(i))
+    const value = e.innerHTML
+    if(value === "") {
+      return false;
+    }
   }
+  return true;
 }
-//check win
- const checkWorWin = () => {
-  if(checkWin()===true) {
-    window.alert(`Player ${currentMarker} won!`)
-  } else {
-    changeMarker()
-  }
-}
-//clear button
